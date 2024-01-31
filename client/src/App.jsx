27c4d43861d2dbd58f2dedfedc461d0accf7c983
daabir mubashir <darmubashir2@gmail.com>
2024-01-31 13:46:46 +0530

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Camera from './components/Camera';
+
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Camera/>}></Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
